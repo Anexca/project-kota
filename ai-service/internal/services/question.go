64462b98342bc82449e57a010cd80ebf7833a6bb
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-27 01:17:43 +0530

    add a common service method to get questions

--- ai-service/internal/services/question.go
+++ ai-service/internal/services/question.go
@@ -14,17 +14,12 @@ type QuestionService struct {
 	redisService *RedisService
 }
 
-type MultipleChoiceQuestion struct {
+type Question struct {
+	Type        string   `json:"type"` // Added field to differentiate MCQ and NVQ
 	Question    string   `json:"question"`
-	Options     []string `json:"options"`
+	Options     []string `json:"options,omitempty"` // Optional for NVQs
 	Answer      string   `json:"answer"`
 	Explanation string   `json:"explanation"`
-}
-
-type NumericalValueQuestion struct {
-	Question    string `json:"question"`
-	Answer      string `json:"answer"`
-	Explanation string `json:"explanation"`
 }
 
 func NewQuestionService(genAIClient *genai.Client, redisClient *redis.Client) *QuestionService {
@@ -38,12 +33,13 @@ func NewQuestionService(genAIClient *genai.Client, redisClient *redis.Client) *Q
 }
 
 const GEN_AI_MODEL = "gemini-1.5-flash"
+const REDIS_CACHE_PREFIX = "QUESTIONS"
 
-func (q *QuestionService) GenerateMCQsForSubject(ctx context.Context, examName, subject string, numberOfQuestions int) (*[]MultipleChoiceQuestion, error) {
-	cachedQuestionKey := fmt.Sprintf("MCQ_%s_%s_%d", examName, subject, numberOfQuestions)
+func (q *QuestionService) GenerateQuestions(ctx context.Context, questionType, examName, subject string, numberOfQuestions int) (*[]Question, error) {
+	cachedQuestionKey := generateCacheKey(questionType, examName, subject, numberOfQuestions)
+	var formattedQuestions []Question
 
-	var formattedQuestions []MultipleChoiceQuestion
-
+	// Check cache for existing questions
 	cachedQuestions, err := q.redisService.Get(ctx, cachedQuestionKey)
 	if err != nil && err != redis.Nil {
 		return nil, err
@@ -59,66 +55,28 @@ func (q *QuestionService) GenerateMCQsForSubject(ctx context.Context, examName, 
 		return &formattedQuestions, nil
 	}
 
-	prompt := fmt.Sprintf(`Generate a JSON array containing %d multiple-choice questions (MCQs) for the %s subject in %s exam. 
-            Each MCQ should have a "question", "options", "answer", and "explanation" field. 
-            The JSON output should be formatted as a single-line string without any extra whitespace or formatting`, numberOfQuestions, subject, examName)
+	// Generate questions using GenAI if not found in cache
+	prompt := fmt.Sprintf(`Generate a JSON array containing %d %s questions for the %s subject in %s exam. 
+                            Each question should have a "type", "question", "answer", "explanation" and "options", field.
+                            The JSON output should be formatted as a single-line string without any extra whitespace or formatting.`,
+		numberOfQuestions, questionType, subject, examName)
 
 	questions, err := q.genAIService.GetContentStream(ctx, prompt, GEN_AI_MODEL)
 	if err != nil {
 		return nil, err
 	}
 
-	err = q.redisService.Store(ctx, cachedQuestionKey, questions)
-	if err != nil {
-		return nil, err
-	}
-
+	// Unmarshall and store questions in cache
 	err = json.Unmarshal([]byte(questions), &formattedQuestions)
 	if err != nil {
 		return nil, err
 	}
+
+	q.redisService.Store(ctx, cachedQuestionKey, questions)
 
 	return &formattedQuestions, nil
 }
 
-func (q *QuestionService) GenerateNVQsForSubject(ctx context.Context, examName, subject string, numberOfQuestions int) (*[]NumericalValueQuestion, error) {
-	cachedQuestionKey := fmt.Sprintf("MCQ_%s_%s_%d", examName, subject, numberOfQuestions)
-	var formattedQuestions []NumericalValueQuestion
-
-	cachedQuestions, err := q.redisService.Get(ctx, cachedQuestionKey)
-	if err != nil && err != redis.Nil {
-		return nil, err
-	}
-
-	if err != redis.Nil {
-		err = json.Unmarshal([]byte(cachedQuestions), &formattedQuestions)
-
-		if err != nil {
-			return nil, err
-		}
-
-		return &formattedQuestions, nil
-	}
-
-	prompt := fmt.Sprintf(`Generate a JSON array containing %d numerical value questions for the %s subject in %s exam. 
-							Each question should have a "question", "answer", and "explanation" field. 
-							The JSON output should be formatted as a single-line string without any extra whitespace or formatting.`,
-		numberOfQuestions, subject, examName)
-
-	questions, err := q.genAIService.GetContentStream(ctx, prompt, GEN_AI_MODEL)
-	if err != nil {
-		return nil, err
-	}
-
-	err = q.redisService.Store(ctx, cachedQuestionKey, questions)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal([]byte(questions), &formattedQuestions)
-	if err != nil {
-		return nil, err
-	}
-
-	return &formattedQuestions, nil
+func generateCacheKey(questionType, examName, subject string, numberOfQuestions int) string {
+	return fmt.Sprintf("%s_%s_%s_%s_%d", REDIS_CACHE_PREFIX, questionType, examName, subject, numberOfQuestions)
 }
