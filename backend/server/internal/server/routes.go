5d0d439cc93453b95cedd626442795c76957be94
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-29 23:33:16 +0530

    setup cors

--- backend/server/internal/server/routes.go
+++ backend/server/internal/server/routes.go
@@ -5,11 +5,23 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Use(middleware.Heartbeat("/ping"))
 
 	r.Get("/sup", s.Sup)
 
