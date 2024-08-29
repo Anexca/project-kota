d87bbe7db66e18aa90dd9657d4a9e9c939289824
nullsploit01 <harshaldharmik099@gmail.com>
2024-08-29 22:28:37 +0530

    initialise server

---
+++ backend/server/internal/server/routes.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/sup", s.Sup)
+
+	return r
+}
+
+func (s *Server) Sup(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Sup"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
