3f1e94f0202b1034185487266f147cd8ad1ee0bf
abdulrahilsheikh <rahilsheikh67064@gmail.com>
2024-08-30 12:17:09 -0700

    initializing ui

---
+++ client/web/src/routes/route.config.ts
@@ -0,0 +1,9 @@
+import { RouteObject } from "react-router-dom";
+import { paths } from "./route.constant";
+
+// const routes: RouteObject[] = [
+//   {
+//     path: paths.HOMEPAGE,
+//     element: <HomePage />,
+//   },
+// ];
---
+++ client/web/src/routes/route.constant.ts
@@ -0,0 +1,9 @@
+export enum paths {
+  HOMEPAGE = "home",
+  STREAM_SELECT = "streams",
+  EXAMS = "exams",
+  DISCRIPTIVE = "discriptive",
+  MCQ = "mcq",
+  LOGIN = "login",
+  REGISTER = "register",
+}
