d44568d13a3db28904a9944ec89a804d2efd1656
nullsploit01 <harshaldharmik099@gmail.com>
2024-09-02 03:47:44 +0530

    add db schema for questions table

--- backend/server/internal/server/banking.go
+++ backend/server/internal/server/banking.go
@@ -11,8 +11,6 @@ const EXAM_CATEGORY_TYPE = commonConstants.Banking
 func (s *Server) GetBankingDescriptiveQuestions(w http.ResponseWriter, r *http.Request) {
 	const EXAM_TYPE = commonConstants.Descriptive
 
-	// var descriptiveQuestions []models.DescriptiveQuestion
-
 	cachedQuestions, err := s.examService.GetQuestionsForExam(r.Context(), EXAM_TYPE)
 	if err != nil {
 		s.ErrorJson(w, err, http.StatusInternalServerError)
