91cb921e5b5b077e37451058c7f4ba5799d1a364
nullsploit01 <harshaldharmik099@gmail.com>
2024-09-14 03:07:16 +0530

    add checks for active subscription

--- backend/server/internal/server/subscription.go
+++ backend/server/internal/server/subscription.go
@@ -1,13 +1,7 @@
 package server
 
 import (
-	"common/ent"
-	"errors"
 	"net/http"
-	"server/pkg/constants"
-	"strconv"
-
-	"github.com/go-chi/chi/v5"
 )
 
 func (s *Server) GetAllSubscriptions(w http.ResponseWriter, r *http.Request) {
@@ -22,36 +16,4 @@ func (s *Server) GetAllSubscriptions(w http.ResponseWriter, r *http.Request) {
 	}
 
 	s.WriteJson(w, http.StatusOK, responsePayload)
-}
-
-func (s *Server) StartSubscription(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "subscriptionId")
-	subscriptionId, err := strconv.Atoi(idParam)
-	if err != nil {
-		s.ErrorJson(w, errors.New("invalid subscription id"), http.StatusBadRequest)
-		return
-	}
-
-	userId, err := GetHttpRequestContextValue(r, constants.UserIDKey)
-	if err != nil {
-		s.ErrorJson(w, errors.New("unauthorized"), http.StatusUnauthorized)
-	}
-
-	userSubscription, err := s.subscriptionService.StartUserSubscription(r.Context(), subscriptionId, userId)
-	if err != nil {
-		var notFoundError *ent.NotFoundError
-		if errors.As(err, &notFoundError) {
-			s.ErrorJson(w, errors.New("subscription not found"))
-			return
-		}
-
-		s.ErrorJson(w, err, http.StatusInternalServerError)
-		return
-	}
-
-	responsePayload := Response{
-		Data: userSubscription,
-	}
-
-	s.WriteJson(w, http.StatusCreated, &responsePayload)
 }
