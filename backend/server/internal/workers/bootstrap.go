0b9db622a1c8198de2c43136ce34d70d23db7b0c
nullsploit01 <harshaldharmik099@gmail.com>
2024-10-07 02:51:57 +0530

    enable server workers

--- backend/server/internal/workers/bootstrap.go
+++ backend/server/internal/workers/bootstrap.go
@@ -2,6 +2,7 @@ package workers
 
 import (
 	"common/ent"
+	"log"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/robfig/cron/v3"
@@ -31,40 +32,40 @@ func InitWorkers(redisClient *redis.Client, dbClient *ent.Client) *cron.Cron {
 
 func (w *Worker) RegisterWorkers() {
 	// _, err := w.cronHandler.AddFunc("*/1 * * * *", func() {
-	// _, err := w.cronHandler.AddFunc("0 4 * * *", func() {
-	// 	log.Println("Starting Worker Job for Adding Descriptive Question in Database")
+	_, err := w.cronHandler.AddFunc("0 4 * * *", func() {
+		log.Println("Starting Worker Job for Adding Descriptive Question in Database")
 
-	// 	// err := w.AddDescriptiveQuestionsInDatabase()
-	// 	// if err != nil {
-	// 	// 	log.Printf("Failed to Add Descriptive Question in Database: %v", err)
-	// 	// }
+		err := w.AddDescriptiveQuestionsInDatabase()
+		if err != nil {
+			log.Printf("Failed to Add Descriptive Question in Database: %v", err)
+		}
 
-	// 	err := w.AddMcqExamsInDatabase()
-	// 	if err != nil {
-	// 		log.Printf("Failed to Add MCQ Exam in Database: %v", err)
-	// 	}
+		err = w.AddMcqExamsInDatabase()
+		if err != nil {
+			log.Printf("Failed to Add MCQ Exam in Database: %v", err)
+		}
 
-	// 	log.Println("Finished Worker Job for Adding Descriptive Question in Database")
-	// })
+		log.Println("Finished Worker Job for Adding Descriptive Question in Database")
+	})
 
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// // w.cronHandler.AddFunc("*/1 * * * *", func() {
-	// _, err = w.cronHandler.AddFunc("0 0 * * 0", func() {
-	// 	log.Println("Starting Worker Job for Creating Descriptive Open Questions")
+	// w.cronHandler.AddFunc("*/1 * * * *", func() {
+	_, err = w.cronHandler.AddFunc("0 0 * * 0", func() {
+		log.Println("Starting Worker Job for Creating Descriptive Open Questions")
 
-	// 	err := w.MarkDescriptiveQuestionsAsOpenInDatabase()
-	// 	if err != nil {
-	// 		log.Printf("Failed to Create Descriptive Open Questions: %v", err)
-	// 		return
-	// 	}
+		err := w.MarkDescriptiveQuestionsAsOpenInDatabase()
+		if err != nil {
+			log.Printf("Failed to Create Descriptive Open Questions: %v", err)
+			return
+		}
 
-	// 	log.Println("Finished Worker Job for Creating Descriptive Open Questions")
-	// })
+		log.Println("Finished Worker Job for Creating Descriptive Open Questions")
+	})
 
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
